8b4b2d2f04d8b826687b4a8120a77d0567863f3c
Kirill Korzun <kirill.korzun.97@gmail.com>
2019-10-13 18:45:32 +0300

    dev(task01_5): implemented fileStorage

--- Potestas/Potestas/Storages/ListStorage.cs
+++ Potestas/Potestas/Storages/ListStorage.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Potestas.Interfaces;
 
 namespace Potestas.Storages
 {
-    public class ListStorage : List<IEnergyObservation>, IEnergyObservationStorage
+    public class ListStorage : List<IEnergyObservation>, IEnergyObservationStorage<IEnergyObservation>
     {
         public string Description => "Simple in-memory storage of energy observations";
+
+        public IEnumerable<IEnergyObservation> GetAll()
+        {
+            return this;
+        }
+
+        public IEnergyObservation GetByHash(int hashCode)
+        {
+            return this.SingleOrDefault(item => item.GetHashCode() == hashCode);
+        }
     }
 }
